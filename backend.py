8b6473d53b20e9a9606682b6391a09af0ebf2564
moonsunghun <msh6618@naver.com>
2025-06-16 10:59:17 +0900

    deploy test 1

--- backend.py
+++ backend.py
@@ -71,7 +71,7 @@ async def chat_endpoint(request: ChatRequest):
 if __name__ == "__main__":
   import uvicorn
   # uvicorn.run(app, host="0.0.0.0", port=8000)
-  uvicorn.run("backend:app", host="127.0.0.1", port=8000, reload=True)
+  uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
 
